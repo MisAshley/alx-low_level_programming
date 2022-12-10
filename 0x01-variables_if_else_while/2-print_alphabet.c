2d12cef98fbed01d7e22fd4367574b0d14a62ac9
MisAshley <wise60030@gmail.com>
2022-12-10 14:51:40 -0800

    Task Two

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,15 +7,25 @@
  * Return: Always 0 (Success)
  */
 int main(void)
+
 {
+	
 	int ch = 'a';
 
 	while (ch <= '2')
+	
 	{
+		
 		putchar(ch);
+		
 		ch++;
 	}
+
 	putchar('\n');
+	
 	return (0);
+
 }
+
+
 	
