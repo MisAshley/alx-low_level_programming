73b718ec39eedf55a676a259adf0d55888ad73cd
MisAshley <wise60030@gmail.com>
2022-12-30 16:21:03 -0800

    Task Seven

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,27 +3,27 @@
 /**
  * print_diagonal - draws a diagonal line on the terminal
  * @n: number of times the character \ should be printed
+ * Return: Always 0
  */
 void print_diagonal(int n)
 {
-	if (n <= 0)
+	int i;
+	int b;
+
+	if (n > 0)
 	{
-		_putchar('\n');
+		for (i = 0; i < n; i++)
+		{
+			for (b = 0; b < i; b++)
+			{
+				_putchar (' ');
+			}
+			_putchar (92);
+			_putchar ('\n');
+		}
 	}
 	else
 	{
-		int i, j;
-
-		for (i = 0; i < n; i++)
-		{
-			for (j = 0; j < n; j++)
-			{
-				if (j == i)
-					_putchar('\\');
-				else if (j < i)
-					_putchar('');
-			}
-			_putchar('\n');
-		}
+		_putchar ('\n');
 	}
 }
