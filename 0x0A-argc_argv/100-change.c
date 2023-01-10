e424fe835e33caf18ea511b60f57c0611e3e4c90
MisAshley <wise60030@gmail.com>
2023-01-09 17:02:28 -0800

    Task Five

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+
 /**
  * main - prints the minimum number of coins to
  * make change for an amount of money
@@ -27,6 +28,7 @@ int main(int argc, char *argv[])
                 printf("0\n");
                 return (0);
         }
+
         for (j = 0; j < 5 && num >= 0; j++)
         {
                 while (num >= coins[j])
