7b3fc7d3e0f49f92ed7c79d1f8b627d0267ded93
MisAshley <wise60030@gmail.com>
2023-01-11 11:18:58 -0800

    Task Five

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,30 +10,30 @@
  */
 int main(int argc, char *argv[])
 {
- int cent_vals[] = {25, 10, 5, 2, 1};
- int cents, number_of_cents, balance, i;
+int cent_vals[] = {25, 10, 5, 2, 1};
+int cents, number_of_cents, balance, i;
 
- if (argc != 2)
- {
-  printf("Error\n");
-  return (1);
- }
- cents = atoi(argv[1]);
- if (cents < 0)
- {
-  printf("0\n");
-  return (0);
- }
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+cents = atoi(argv[1]);
+if (cents < 0)
+{
+printf("0\n");
+return (0);
+}
 
- number_of_cents = 0;
- balance = cents;
- for (i = 0; i < 5; ++i)
- {
-  number_of_cents += balance / cent_vals[i];
-  if ((balance % cent_vals[i]) == 0)
-   break;
-  balance %= cent_vals[i];
- }
- printf("%d\n", number_of_cents);
- return (0);
+number_of_cents = 0;
+balance = cents;
+for (i = 0; i < 5; ++i)
+{
+number_of_cents += balance / cent_vals[i];
+if ((balance % cent_vals[i]) == 0)
+ break;
+balance %= cent_vals[i];
+}
+printf("%d\n", number_of_cents);
+return (0);
 }
