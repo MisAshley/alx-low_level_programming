d8e28ad10eede4b21abc4946dd9d4471e459b774
MisAshley <wise60030@gmail.com>
2023-01-14 14:33:10 -0800

    Task Zero

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,14 +1,18 @@
 #include "main.h"
-void _puts(char *str)
-#include<stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * _puts - prints a string followed by a newline
+ * to stdout
+ * @str: parameter that contains a string
+ * Return: void
  */
-int main(void)
+void _puts(char *str)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
+int i = 0;
 
+while (str[i])
+{
+_putchar(str[i]);
+i++;
+}
+_putchar('\n');
+}
